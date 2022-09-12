39f27316823e45698b769d28394284003f2578dc
kengmzx <113336256+kengmzx@users.noreply.github.com>
2022-09-12 12:12:35 -0600

    Add files via upload

---
+++ Periodo1Clase6.2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+int division, numerol, numero2;
+cout << "Ingrese el primer numero: ";
+cin >> numero1;
+cout << "Ingrese el segundo numero: ";
+cin >> numero2;
+if (numero1 == 0) {
+   cout << "Error El primer numero es cero";
+}else if (numero2 == 0) {
+cout << "Error El segundo numero es cero";
+} else {
+division = numerol / numero2;
+cout << "La division es:" << division;
+  }
+}
---
+++ Periodo3Clase13.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main(){ 
+int n = 1234;
+int reverseOfn = 0;
+while (n > 0) { //test expression
+// update expression
+n = n / 10;
+}
+cout << "Reverse of n i: " << reverseOfn;
+}
